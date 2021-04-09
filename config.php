66656b7095948e82538376a9907cc89817512ad9
patels183 <74590893+patels183@users.noreply.github.com>
2021-04-09 08:17:45 -0400

    keep trying

--- config.php
+++ config.php
@@ -8,4 +8,9 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "patels18", "pwd" => "Cloudif9bre", "Database" => "patels18_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:localhost17.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
